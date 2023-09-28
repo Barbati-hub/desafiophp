b4f65e027bc2d663420f068971bbcfa6155be5d5
Barbati-hub <douglas-ltda@hotmail.com.br>
2023-09-28 05:22:25 -0400

    alterações

--- dia4/exercicio2.php
+++ dia4/exercicio2.php
@@ -18,32 +18,38 @@
 
 // depois voltar para o menu e continuar no mesmo até que o aluno digite a opção 5
 
-echo "========== MENU ==========" . "\n";
-echo " 1 - Cadastrar Aluno
-2 - Alterar Aluno
-3 - Excluir Aluno
-4 - Mostrar todos os Alunos
-5 - Sair
-";
 
-echo "Escolha uma opção: ";
-$opcao = fgets(STDIN);
-
-switch ($opcao) {
-    case 1:
-        echo "Nome do Aluno: ";
-
-        echo "Endereço:";
-        break;
-    case 2:
-        echo "Alterar Aluno";
-        echo "Nome do Aluno a ser Alterado";
-        break;
-    case 3:
-        echo "Excluir Aluno";
-        break;
-    case 4:
-        echo "Alunos Cadastrados";
-    default:
-        echo "Saindo";
+while(true){
+    echo "========== MENU ==========" . "\n";
+    echo " 1 - Cadastrar Aluno
+    2 - Alterar Aluno
+    3 - Excluir Aluno
+    4 - Mostrar todos os Alunos
+    5 - Sair
+    ";
+    
+    echo "Escolha uma opção: ";
+    $opcao = fgets(STDIN);
+    
+    switch ($opcao) {
+        case 1:
+            echo "Nome do Aluno: ";
+    
+            echo "Endereço:";
+            break;
+        case 2:
+            echo "Alterar Aluno";
+            echo "Nome do Aluno a ser Alterado";
+            break;
+        case 3:
+            echo "Excluir Aluno";
+            break;
+        case 4:
+            echo "Alunos Cadastrados";
+            break;
+        default:
+            echo "Saindo";
+            exit();
+    }
 }
+
