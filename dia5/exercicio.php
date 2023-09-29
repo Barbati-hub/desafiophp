43af89bf340fe188c2919ac8ded2ee7a9a0870cd
Barbati-hub <douglas-ltda@hotmail.com.br>
2023-09-29 05:35:56 -0400

    upgrade

--- dia5/exercicio.php
+++ dia5/exercicio.php
@@ -1,27 +1,57 @@
 <?php
-echo "================================================" . "\n";
-echo "==========  CADASTRO DE PROFISSIONAIS  =========" . "\n";
- "\n";
- "\n";
-echo "Digite o nome do profissional:" . "\n";
-$nomeProfissional = fgets(STDIN);
-echo "Digite o Cargo do Profissional: ". "\n";
-$cargoProfissional = fgets(STDIN);
-echo "Digite o Valor do Profissional: ". "\n";
-$valorProfissional = fgets(STDIN);
 
-$profissional = [
-    array(
-    "nome" => $nomeProfissional,
-    "cargo" => $cargoProfissional,
-    "valor" => $valorProfissional
-    ),
-];
+while(true){
+    echo "
+    1 - Cadastrar Profissional
+    2 - Alterar Profissional
+    3 - Excluir Profissional" . "\n";
+    
 
-foreach ($profissional as $profissionais){
-    echo "=== DADOS DOS PROFISSIONAIS === \n";
-    "\n";
-    echo "Nome: {$profissionais["nome"]} \n";
-    echo "Cargo: {$profissionais["cargo"]} \n";
-    echo "Valor: {$profissionais["valor"]} \n";
-}
+
+    echo "Escolha uma Opção Acima: ";
+    $opcao = fgets(STDIN);
+    
+    switch($opcao){
+        case 1: {
+            echo "================================================" . "\n";
+            echo "==========  CADASTRO DE PROFISSIONAIS  =========" . "\n";
+            "\n";
+            "\n";
+            echo "Digite o nome do profissional:" . "\n";
+            $nomeProfissional = fgets(STDIN);
+            echo "Digite o Cargo do Profissional: ". "\n";
+            $cargoProfissional = fgets(STDIN);
+            echo "Digite o Valor do Profissional: ". "\n";
+            $valorProfissional = fgets(STDIN);
+            break;
+        };
+        case 2: {
+            echo "Alterar";
+            break;
+        };
+        case 3: {
+            echo "Excluir";
+            break;
+        };
+        case 5: {
+            exit();
+        };
+    }
+
+    $profissional = [
+        array(
+        "nome" => $nomeProfissional,
+        "cargo" => $cargoProfissional,
+        "valor" => $valorProfissional
+        ),
+    ];
+    
+    foreach ($profissional as $profissionais){
+        echo "=== DADOS DOS PROFISSIONAIS === \n";
+        "\n";
+        echo "Nome: {$profissionais["nome"]} \n";
+        echo "Cargo: {$profissionais["cargo"]} \n";
+        echo "Valor: {$profissionais["valor"]} \n";
+    }
+    
+};
