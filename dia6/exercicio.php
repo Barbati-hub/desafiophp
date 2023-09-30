5229f6aaf447e777daee3c2d06fee97c67d52401
Barbati-hub <douglas-ltda@hotmail.com.br>
2023-09-30 05:46:46 -0400

    dia06 com listagem

--- dia6/exercicio.php
+++ dia6/exercicio.php
@@ -1,6 +1,9 @@
 <?php
 
+$registerProduct = [];
+
 function register(){
+     global $registerProduct; 
     echo "Digite o Código do Produto: ";
     $id = fgets(STDIN);
 
@@ -13,45 +16,65 @@ function register(){
     echo "Digite a Quantidade: ";
     $qdte = fgets(STDIN);
 
+    $ProductRegister = [
+        "id" => $id,
+        "Produto" => $product,
+        "Descrição" => $description,
+        "Quantidade" => $qdte
+    
+];
+    $registerProduct [] = $ProductRegister;
+    echo "Produto cadastrado com sucesso!\n";
+
 }
 
 function listProduct(){
-    $registerProduct = [
-        array(
-            "id" => $id,
-            "Produto" => $product,
-            "Descrição" => $description,
-            "Quantidade" => $qdte
-        )
-    ];
+   global $registerProduct;
+   echo "=== ESTOQUE ===\n";
+   foreach ($registerProduct as $product){
+        echo "\n";
+        echo "ID: {$product["id"]}\n";
+        echo "Produto: {$product["Produto"]}\n";
+        echo "Descrição: {$product["Descrição"]}\n";
+        echo "Quantidade: {$product["Quantidade"]}\n";
+    }
 
-    foreach ($registerProduct as $products){
-        echo "=== ESTOQUE === \n";
-        "\n";
-        echo "ID: {$products["id"]} \n";
-        echo "Produto: {$products["Produto"]} \n";
-        echo "Descricão: {$products["Descrição"]} \n";
-        echo "Quantidade: {$products["Quantidade"]} \n";
+  
+}
+function contagemRegressiva(){
+    for($i = 5; $i >0  ; $i--){
+        sleep(1);
+        echo "Saindo em: $i"  . "\n";
     }
 }
 
 
 
 while(true){
-    echo "
-    1 - Cadastrar Produto
-    2 - Listar Produto
-    ";
-    echo "Escolha uma Opção Acima: ";
-    $opcao = fgets(STDIN);
-    switch($opcao){
-        case 1: {
-            register();
-            break;
+    echo "Digite S (SIM) para Cadastrar outro Profissional N (NÃO) para SAIR ";
+    $cadastro = trim(fgets(STDIN));
+
+    if (strcasecmp($cadastro, "S") == 0){
+        echo "
+        1 - Cadastrar Produto
+        2 - Listar Produto
+        ";
+        echo "Escolha uma Opção Acima: ";
+        $opcao = fgets(STDIN);
+        switch($opcao){
+            case 1: {
+                register();
+                break;
+            };
+            case 2: {
+                listProduct();
+                break;
+            };
         };
-        case 2: {
-            listProduct();
-            break;
-        };
-    };
+    }
+    if (strcasecmp($cadastro, "N") == 0){
+        contagemRegressiva();
+        exit();
+    }
+    
 }
